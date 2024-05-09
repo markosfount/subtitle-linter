ca7709bda83b1c2b41fed8362965f589738a9523
markosfount <sc.markos@gmail.com>
2024-05-09 15:19:32 +0300

    cleanup

--- main.go
+++ main.go
@@ -13,14 +13,6 @@ var regChar = regexp.MustCompile("(^.+: )")
 var regPar = regexp.MustCompile("(\\(.+\\))")
 
 func breakLongLines(filePath string) {
-	//dat, err := os.ReadFile(filePath)
-	//detector := chardet.NewTextDetector()
-	//result, err := detector.DetectBest(dat)
-	//if err != nil {
-	//	fmt.Println("Error:", err)
-	//	return
-	//}
-	//fmt.Println(result)
 	file, err := os.Open(filePath)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -28,15 +20,10 @@ func breakLongLines(filePath string) {
 	}
 	defer file.Close()
 
-	//dec := transform.NewReader(file, charmap.Windows1252.NewDecoder())
-
 	scanner := bufio.NewScanner(file)
 	var modifiedLines []string
 	for scanner.Scan() {
 		line := scanner.Text()
-		//line2 := scanner.Bytes()
-		//ls := string(line2)
-		//fmt.Println(ls)
 		isSub := regChar.MatchString(line)
 		isTime := strings.Contains(line, "TIME")
 		if isSub || isTime {
@@ -45,7 +32,7 @@ func breakLongLines(filePath string) {
 		line = regChar.ReplaceAllString(line, "")
 		line = regPar.ReplaceAllString(line, "")
 
-		if length(line) > 25 { //&& regChar.MatchString(line) {
+		if length(line) > 25 {
 			words := strings.Fields(line)
 			var currentLine string
 			for _, word := range words {
@@ -89,7 +76,6 @@ func length(line string) int {
 }
 
 func main() {
-	//filePath := "/home/markos/Documents/11th Draft.txt" // Change this to your file path
-	filePath := "11th Draft.txt" // Change this to your file path
+	filePath := "11th Draft.txt"
 	breakLongLines(filePath)
 }
